951a13138b2bd9b369d34d18c9276e9bc389d4b7
Riku Kaipainen <shylessfly@gmail.com>
2017-12-02 01:46:58 +0200

    Trying out PDCurses functions, some template done

--- Project/Roguelike/Roguelike.cpp
+++ Project/Roguelike/Roguelike.cpp
@@ -1,21 +1,31 @@
-// Roguelike.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "curses.h"
 
 int main() {
-	initscr();										 /* Initialize screenbuffer */
+	initscr();										/* Start curses */
+	resize_term(23, 56);							/* Set initial window size */
+	WINDOW *status,*game, *textBar, *gameBox;		/* Initialize 3 windows for status,text and game */
+	status = newwin(20, 15, 3, 0);
+	game = newwin(20, 40, 3, 16);
+	textBar = newwin(3, 55, 0, 0);					/* new window with size = (Columns, Rows, start y, start x) */
 	
-	if (has_colors) {								 /* If console has colors */
-		start_color();								 /* Initialize colors*/
-		init_pair(1, COLOR_BLACK, COLOR_BLACK);      /* Example of block with black text and bg */
-	}		
+	do {
+		for (int i = 0; i < 38; i++) {
+			for (int j = 0; j < 39; j++)
+				mvwaddch(game, i, j, '.');
+		}
+		mvwprintw(textBar, 1, 1, "Text bar > ");
+		mvwprintw(status, 1, 1, "Status bar");
+		box(status, '|', '-');
+		box(textBar, '|', '-');						/* read stuff to window buffers */
+		wrefresh(game);								/* Refresh draws buffer to actual screen */
+		wrefresh(status);
+	} while (wgetch(textBar));						/* wgetch also refreshes selected window */
 	
-	resize_term(30, 70);							 /* Resizes screenbuffer size of 30 y 70 x */
+	delwin(status);
+	delwin(game);
+	delwin(textBar);
+	endwin();										/* Delete window buffer memories */
 
-	refresh();
-	getch();
-	endwin();
 	return 0;
 }
