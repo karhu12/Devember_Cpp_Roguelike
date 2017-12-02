f2c0416a308ecbe601b46a4576e11d88eaccfed1
Riku Kaipainen <shylessfly@gmail.com>
2017-12-02 13:44:14 +0200

    Testing out functions and structure

--- Project/Roguelike/Roguelike.cpp
+++ Project/Roguelike/Roguelike.cpp
@@ -1,19 +1,21 @@
 ﻿#include "stdafx.h"
 #include "curses.h"
 #include <string>
+#include <iostream>
 
 class _buffer {
 public:
-	WINDOW *statusWindow, *gameWindow, *textWindow;										/* Initialize 3 windows for status,text and game */	
-	void drawBorders(WINDOW * status, WINDOW * textBar);								/* draw borders */
-	void bufferRelease(WINDOW * first, WINDOW * second, WINDOW * third);				/* Release buffer memory */
-	void drawStatus(WINDOW * status);													/* Draw status window specific */
+	WINDOW *statusWindow, *gameWindow, *textWindow;								/* Initialize 3 windows for status,text and game */	
+	void drawBorders(WINDOW * status, WINDOW * textBar);						/* draw borders */
+	void bufferRelease(WINDOW * first, WINDOW * second, WINDOW * third);		/* Release buffer memory */
+	void drawStatus(WINDOW * status);											/* Draw status window specific */
 };
 
 int main() {
 	_buffer buffer;									/* Initialize buffer object */
 	initscr();										/* Start curses */
-	if (has_colors) {								/* Initialize basic colors */
+	curs_set(1);
+	if (has_colors() == TRUE) {						/* Initialize basic colors */
 		start_color();
 		init_pair(1, COLOR_CYAN, COLOR_BLACK);
 		init_pair(2, COLOR_MAGENTA, COLOR_BLACK);
@@ -27,9 +29,11 @@ int main() {
 	buffer.statusWindow = newwin(20, 20, 5, 0);
 	buffer.gameWindow = newwin(18, 60, 6, 23);
 	buffer.textWindow = newwin(5, 85, 0, 0);		/* Create game windows of approppriate size */
+
 	buffer.drawBorders(buffer.statusWindow, buffer.textWindow);
-	buffer.drawStatus(buffer.statusWindow);
-	for (int i = 0; i < 18; i++) {
+	buffer.drawStatus(buffer.statusWindow);			/* Buffer draw functions */
+
+	for (int i = 0; i < 18; i++) {					/* Example of game field drawn from 0,0 coordinate */
 		for (int j = 0; j < 60; j++) {
 			wattron(buffer.gameWindow, COLOR_PAIR(4));
 			mvwaddch(buffer.gameWindow, i, j, ACS_CKBOARD);
@@ -37,20 +41,22 @@ int main() {
 		}
 	}
 	wrefresh(buffer.gameWindow);
-	wmove(buffer.textWindow, 3, 1);
-	wgetch(buffer.textWindow);
+	mvwprintw(buffer.textWindow, 1, 2, "Press any key to quit...");
+	mvwgetch(buffer.textWindow, 3, 1);
+		
+													/* Release window buffer memory */
 	buffer.bufferRelease(buffer.statusWindow, buffer.gameWindow, buffer.textWindow);
 	return 0;
 }
 
 void _buffer::drawBorders(WINDOW * status, WINDOW * text) {
 	box(status, ACS_VLINE, ACS_HLINE);
-	box(text, ACS_VLINE, ACS_HLINE);						/* draw borders to window buffers */
+	box(text, ACS_VLINE, ACS_HLINE);				/* draw borders to window buffers */
 	wrefresh(status);
-	wrefresh(text);								/* Refresh draws buffer to the actual screen */
+	wrefresh(text);									/* Refresh draws buffer to the actual screen */
 }
 
-void _buffer::drawStatus(WINDOW * status) {
+void _buffer::drawStatus(WINDOW * status) {			/* Draw text on status bar (later on add actual variables) */
 	wattron(status, A_BOLD);
 	mvwprintw(status, 2, 2, "PLAYER");
 	wattroff(status, A_BOLD);
@@ -90,13 +96,12 @@ void _buffer::drawStatus(WINDOW * status) {
 	wattroff(status, COLOR_PAIR(3));
 	wattroff(status, A_BOLD);
 	
-
-	wrefresh(status);
+	wrefresh(status);							
 }
 
 void _buffer::bufferRelease(WINDOW * first, WINDOW * second, WINDOW * third) {
 	delwin(first);
 	delwin(second);
 	delwin(third);
-	endwin();									/* free all window buffer memory */
+	endwin();									/* free every windows buffer memory */
 }
--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Devember C++ Roguelike
-This is a repository for a project where we aim to create a text based roguelike using c++.
+This is a repository for a project where we aim to create a roguelike using c++.
 We will be using PDCurses library to work with command prompt as our program display.
 
 Solemn Swear
 ------------
 We will participate to the next Devember. 
-Our Devember will be creating a text based roguelike on C++.
+Our Devember will be creating a roguelike on C++.
 I promise I will program for my Devember for at least an hour, every day of the next December.
 I will also write a daily public devlog and will make the produced code publicly available on the internet.
 No matter what, I will keep my promise. 
 
-* We will try to create text based roguelike console game using C++
+* We will try to create roguelike console game using C++
+* Should have procedually generated maps
+* RPG elements (npcs, overworld, etc.)
+* 
 
 1.12.2017
 ---------
