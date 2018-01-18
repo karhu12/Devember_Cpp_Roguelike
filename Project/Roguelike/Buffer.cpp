8b75046b26acd8f61d7d86248cdd45a988b3e89a
Riku <shylessfly@gmail.com>
2018-01-18 08:16:18 +0200

    Removed some overly used this operators

--- Project/Roguelike/Buffer.cpp
+++ Project/Roguelike/Buffer.cpp
@@ -8,85 +8,85 @@ void Buffer::drawGame(Map *map, Tile tile[], character *player) {
 		for (int j = 0; j < AREA_MAX_WIDTH; j++) {
 			if (i == player->yPos && j == player->xPos) {		/* If position is players position, draw player */
 				x = returnColorPair(player->foregroundColor, tile[map->area[i][j]].backgroundColor);
-				wattron(this->gameWindow, A_BOLD);
-				wattron(this->gameWindow, COLOR_PAIR(x));
-				mvwaddch(this->gameWindow, i, j, player->playerCharacter);
-				wattroff(this->gameWindow, COLOR_PAIR(x));
-				wattroff(this->gameWindow, A_BOLD);
+				wattron(gameWindow, A_BOLD);
+				wattron(gameWindow, COLOR_PAIR(x));
+				mvwaddch(gameWindow, i, j, player->playerCharacter);
+				wattroff(gameWindow, COLOR_PAIR(x));
+				wattroff(gameWindow, A_BOLD);
 			}
 			else {												/* Else draw tile */
 				x = returnColorPair(tile[map->area[i][j]].foregroundColor, tile[map->area[i][j]].backgroundColor);
-				wattron(this->gameWindow, A_BOLD);
-				wattron(this->gameWindow, COLOR_PAIR(x));
-				mvwaddch(this->gameWindow, i, j, tile[map->area[i][j]].tileCharacter);
-				wattroff(this->gameWindow, COLOR_PAIR(x));
-				wattroff(this->gameWindow, A_BOLD);
+				wattron(gameWindow, A_BOLD);
+				wattron(gameWindow, COLOR_PAIR(x));
+				mvwaddch(gameWindow, i, j, tile[map->area[i][j]].tileCharacter);
+				wattroff(gameWindow, COLOR_PAIR(x));
+				wattroff(gameWindow, A_BOLD);
 			}
 		}
 	}
-	wrefresh(this->gameWindow);									/* Refresh buffer */
+	wrefresh(gameWindow);									/* Refresh buffer */
 }
 
 void Buffer::listCommands() {
 	noecho();
 	attron(A_BOLD);
-	mvwprintw(this->textWindow, 1, 2, "COMMANDS | ");
-	wColorOn(this->textWindow, COLOR_GREEN, COLOR_BLACK);
-	wprintw(this->textWindow, "w / a / s / d ");
-	wColorOff(this->textWindow, COLOR_GREEN, COLOR_BLACK);
-	wprintw(this->textWindow, "for player movements | ");
-	wColorOn(this->textWindow, COLOR_RED, COLOR_BLACK);
-	wprintw(this->textWindow, "q");
-	wColorOff(this->textWindow, COLOR_RED, COLOR_BLACK);
-	wprintw(this->textWindow, " to quit | ");
-	mvwprintw(this->textWindow, 3, 2, "Press any key to return ");
-	wgetch(this->textWindow);
-	wclear(this->textWindow);
+	mvwprintw(textWindow, 1, 2, "COMMANDS | ");
+	wColorOn(textWindow, COLOR_GREEN, COLOR_BLACK);
+	wprintw(textWindow, "w / a / s / d ");
+	wColorOff(textWindow, COLOR_GREEN, COLOR_BLACK);
+	wprintw(textWindow, "for player movements | ");
+	wColorOn(textWindow, COLOR_RED, COLOR_BLACK);
+	wprintw(textWindow, "q");
+	wColorOff(textWindow, COLOR_RED, COLOR_BLACK);
+	wprintw(textWindow, " to quit | ");
+	mvwprintw(textWindow, 3, 2, "Press any key to return ");
+	wgetch(textWindow);
+	wclear(textWindow);
 	echo();
 }
 
 void Buffer::drawStatus(character * player) {	/* Draw player status */
-	wclear(this->statusWindow);
-	box(this->statusWindow, ACS_VLINE, ACS_HLINE);
-	wattron(this->statusWindow, A_BOLD);
-	mvwprintw(this->statusWindow, 2, 2, "PLAYER");
-	wColorOn(this->statusWindow, COLOR_RED, COLOR_BLACK);
-	mvwprintw(this->statusWindow, 4, 2, "HP");
-	wColorOff(this->statusWindow, COLOR_RED, COLOR_BLACK);
-	wColorOn(this->statusWindow, COLOR_YELLOW, COLOR_BLACK);
-	mvwprintw(this->statusWindow, 5, 2, "LEVEL");
-	mvwprintw(this->statusWindow, 6, 2, "EXP");
-	wColorOff(this->statusWindow, COLOR_YELLOW, COLOR_BLACK);
-	mvwprintw(this->statusWindow, 4, 10, "%d", player->health);
-	mvwprintw(this->statusWindow, 5, 10, "%d", player->level);
-	mvwprintw(this->statusWindow, 6, 10, "%d", player->experience);
-	mvwprintw(this->statusWindow, 8, 2, "ATTRIBUTES");
-	wColorOn(this->statusWindow, COLOR_RED, COLOR_RED);
-	mvwprintw(this->statusWindow, 10, 2, "STR");
-	wColorOff(this->statusWindow, COLOR_RED, COLOR_RED);
-	wColorOn(this->statusWindow, COLOR_GREEN, COLOR_GREEN);
-	mvwprintw(this->statusWindow, 11, 2, "DEX");
-	wColorOff(this->statusWindow, COLOR_GREEN, COLOR_GREEN);
-	wColorOn(this->statusWindow, COLOR_BLUE, COLOR_BLUE);
-	mvwprintw(this->statusWindow, 12, 2, "INT");
-	wColorOff(this->statusWindow, COLOR_BLUE, COLOR_BLUE);
-	mvwprintw(this->statusWindow, 10, 10, "%d", player->strength);
-	mvwprintw(this->statusWindow, 11, 10, "%d", player->dexterity);
-	mvwprintw(this->statusWindow, 12, 10, "%d", player->intelligence);
-	wattroff(this->statusWindow, A_BOLD);
-	wrefresh(this->statusWindow);
+	wclear(statusWindow);
+	box(statusWindow, ACS_VLINE, ACS_HLINE);
+	wattron(statusWindow, A_BOLD);
+	mvwprintw(statusWindow, 2, 2, "PLAYER");
+	wColorOn(statusWindow, COLOR_RED, COLOR_BLACK);
+	mvwprintw(statusWindow, 4, 2, "HP");
+	wColorOff(statusWindow, COLOR_RED, COLOR_BLACK);
+	wColorOn(statusWindow, COLOR_YELLOW, COLOR_BLACK);
+	mvwprintw(statusWindow, 5, 2, "LEVEL");
+	mvwprintw(statusWindow, 6, 2, "EXP");
+	wColorOff(statusWindow, COLOR_YELLOW, COLOR_BLACK);
+	mvwprintw(statusWindow, 4, 10, "%d", player->health);
+	mvwprintw(statusWindow, 5, 10, "%d", player->level);
+	mvwprintw(statusWindow, 6, 10, "%d", player->experience);
+	mvwprintw(statusWindow, 8, 2, "ATTRIBUTES");
+	wColorOn(statusWindow, COLOR_RED, COLOR_RED);
+	mvwprintw(statusWindow, 10, 2, "STR");
+	wColorOff(statusWindow, COLOR_RED, COLOR_RED);
+	wColorOn(statusWindow, COLOR_GREEN, COLOR_GREEN);
+	mvwprintw(statusWindow, 11, 2, "DEX");
+	wColorOff(statusWindow, COLOR_GREEN, COLOR_GREEN);
+	wColorOn(statusWindow, COLOR_BLUE, COLOR_BLUE);
+	mvwprintw(statusWindow, 12, 2, "INT");
+	wColorOff(statusWindow, COLOR_BLUE, COLOR_BLUE);
+	mvwprintw(statusWindow, 10, 10, "%d", player->strength);
+	mvwprintw(statusWindow, 11, 10, "%d", player->dexterity);
+	mvwprintw(statusWindow, 12, 10, "%d", player->intelligence);
+	wattroff(statusWindow, A_BOLD);
+	wrefresh(statusWindow);
 }
 
 void Buffer::drawText() {
-	wclear(this->textWindow);
-	box(this->textWindow, ACS_VLINE, ACS_HLINE);
-	wrefresh(this->textWindow);
+	wclear(textWindow);
+	box(textWindow, ACS_VLINE, ACS_HLINE);
+	wrefresh(textWindow);
 }
 
 void Buffer::drawEncounter() {
-	wclear(this->gameWindow);
-	box(this->gameWindow, ACS_VLINE, ACS_HLINE);
-	wmove(this->gameWindow, 24, 1);
-	whline(this->gameWindow, ACS_HLINE, AREA_MAX_WIDTH - 2);
-	wrefresh(this->gameWindow);
+	wclear(gameWindow);
+	box(gameWindow, ACS_VLINE, ACS_HLINE);
+	wmove(gameWindow, 24, 1);
+	whline(gameWindow, ACS_HLINE, AREA_MAX_WIDTH - 2);
+	wrefresh(gameWindow);
 }
